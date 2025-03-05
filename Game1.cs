b01bfcaea3823fe7468a16cbe96ca2ac56d0254c
sofiia-maksymenko <sofiia2511@gmail.com>
2025-03-05 23:54:50 +0100

    Block oben zeichnen

--- Game1.cs
+++ Game1.cs
@@ -10,18 +10,10 @@ namespace Tetris
 
         Texture2D block;
         int blockSize = 20;
-        int y = 0; 
-        int ground;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-        }
-
-        protected override void Initialize()
-        {
-            ground = graphics.PreferredBackBufferHeight - blockSize;
-            base.Initialize();
         }
 
         protected override void LoadContent()
@@ -35,22 +27,12 @@ namespace Tetris
             block.SetData(data);
         }
 
-        protected override void Update(GameTime gameTime)
-        {
-            if (y < ground)
-                y += 2;
-
-            base.Update(gameTime);
-        }
-
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
-            spriteBatch.Draw(block, new Vector2(100, y), Color.White);
+            spriteBatch.Draw(block, new Vector2(100, 0), Color.White);
             spriteBatch.End();
-
-            base.Draw(gameTime);
         }
     }
 }
